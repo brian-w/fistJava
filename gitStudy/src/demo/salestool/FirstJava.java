45dec53fcbe5d158c9fe1769c0a6c2d75b7e1a1f
Brian <zengguangwang@gmail.com>
2014-07-24 15:52:47 -0700

    Initial checkin Test

---
+++ gitStudy/src/demo/salestool/FirstJava.java
@@ -0,0 +1,11 @@
+package demo.salestool;
+
+public class FirstJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World!");
+	}
+
+}
